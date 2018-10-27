004b069f78ab1e7d6be14c0ccd77cc633f261564
NiallD565 <G00338932@gmit.ie>
2018-10-27 17:50:55 +0100

    Wave spawing implemented

--- Defeat_Them_All/Assets/_Scripts/PointSpawner.cs
+++ Defeat_Them_All/Assets/_Scripts/PointSpawner.cs
@@ -14,6 +14,16 @@ public class PointSpawner : MonoBehaviour
     private float waveTimer = 30.0f;
     [SerializeField]
     private float timeTillWave = 5.0f;
+    [SerializeField]
+    private int totalWaves = 5;
+
+    private int numWaves = 0;
+    private bool waveSpawn = false;
+    private int numEnemy = 0;
+    private int spawnedEnemy = 0;
+
+    private GameObject enemyParent;
+
 
     [SerializeField]
     private float spawnDelay = 0.5f;
@@ -24,46 +34,75 @@ public class PointSpawner : MonoBehaviour
     //private Enemy enemyPrefab;
 
     // list for the child SpawnPoints
-    private IList<SpawnPoint> spawnPoints;
+    //private IList<SpawnPoint> spawnPoints;
 
-    private Stack<SpawnPoint> spawnStack;
+    //private Stack<SpawnPoint> spawnStack;
 
 
     // Use this for initialization
     void Start()
     {
         // get the list of child object
-        spawnPoints = GetComponentsInChildren<SpawnPoint>();
-        SpawnEnemiesRepeating();
+        //spawnPoints = GetComponentsInChildren<SpawnPoint>();
+        //SpawnEnemiesRepeating();
+        enemyParent = ParentUtils.FindEnemyParent();
     }
 
-    private void SpawnEnemiesRepeating()
+    /*private void SpawnEnemiesRepeating()
     {
         // shuffle my stack first (from Utilities namespace)
-        spawnStack = ListUtility.CreateShuffledStack(spawnPoints);
+        //spawnStack = ListUtility.CreateShuffledStack(spawnPoints);
         InvokeRepeating(SPAWN_METHOD, spawnDelay, spawnInterval);
     }
-
-    private void Spawn()
+    */
+    private void SpawnEnemy()
     {
         // take spawn point from stack
         // if the stack is empty, then reshuffle 
-        if (spawnStack.Count == 0)
-        {
-            // reshuffle
-            spawnStack = ListUtility.CreateShuffledStack(spawnPoints);
-        }
 
-        var spawnPoint = spawnStack.Pop();
-        
-        var enemy = Instantiate(enemyPrefab);
+        // reshuffle
+        //spawnStack = ListUtility.CreateShuffledStack(spawnPoints);
+
+
+        //var spawnPoint = spawnStack.Pop();
+        Enemy enemy = Instantiate(enemyPrefab, enemyParent.transform.position);
+        enemy.transform.position = transform.position;
+        Rigidbody2D rb = enemy.GetComponent<Rigidbody2D>();
+        rb.velocity = Vector3.down;
+
+        //var enemy = Instantiate(enemyPrefab);
         // set the position
-        enemy.transform.position = spawnPoint.transform.position;
+        //enemy.transform.position = spawnPoint.transform.position;
 
     }
     // Update is called once per frame
     void Update()
     {
-
+		// Spawns enemies in waves but based on time.
+        // checks if the number of waves is bigger than the total waves
+        if (numWaves <= totalWaves)
+        {
+            // Increases the timer to allow the timed waves to work
+            timeTillWave += Time.deltaTime;
+            if (waveSpawn)
+            {
+                //spawns an enemy
+                SpawnEnemy();
+            }
+            // checks if the time is equal to the time required for a new wave
+            if (timeTillWave >= waveTimer)
+            {
+                // enables the wave spawner
+                waveSpawn = true;
+                // sets the time back to zero
+                timeTillWave = 0.0f;
+                // increases the number of waves
+                numWaves++;
+                // A hack to get it to spawn the same number of enemies regardless of how many have been killed
+                numEnemy = 0;
+            }
+        }
     }
-}
+} 
+    
+
