60db096855e587a6f784c6c2e0759a3546ea4e38
NiallD565 <G00338932@gmit.ie>
2018-11-13 13:31:00 +0000

    Addition over GameOver and Pause Menus

---
+++ Defeat_Them_All/Assets/_Scripts/EndGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour {
+
+    public GameObject PauseMenuPanel; //holds the user interface
+    public GameObject GameOverPanel; //holds the user interface
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Quit()
+    {
+        Debug.Log("GameEnded");
+        PauseMenuPanel.SetActive(false);
+        GameOverPanel.SetActive(true);
+
+    }
+}
---
+++ Defeat_Them_All/Assets/_Scripts/Pause.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+
+    public static bool GamePause = false;
+
+    public GameObject PauseMenuPanel; //holds the user interface
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePause == true)
+            {
+                ResumeGame();
+
+            }
+            else
+                PauseGame();
+        }
+
+    }
+
+    public void PauseGame()
+    {
+        Debug.Log("teest");
+        //when pause want to set the pauseGame = true
+        PauseMenuPanel.SetActive(true);
+        Time.timeScale = 0.0f;// stopping time
+        GamePause = true;
+    }
+
+    public void ResumeGame()
+    {
+        PauseMenuPanel.SetActive(false);
+        Time.timeScale = 1.0f;
+        GamePause = false;
+    }
+}
